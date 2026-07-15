7631db1acd4466ca88be432eff273929c7a5f541
Mayank Paswan <58441397+basileus07@users.noreply.github.com>
2026-07-15 13:39:49 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.02%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int __gcd(int a, int b) {
+        while (b != 0) {
+            int rem = a % b;
+            a = b;
+            b = rem;
+        }
+
+        return a;
+    }
+    int gcdOfOddEvenSums(int n) {
+        // here sum of AP -> S(n) = n/2[a+(n-1)d]  or n/2[a+l]
+
+        // for n odd = n^2
+        // for n even = n^2+n
+        int a = n * n;
+        int b = a + n;
+
+        return __gcd(a, b);
+    }
+};
